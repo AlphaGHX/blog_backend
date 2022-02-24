a11af3e5a899223a08958639e09b7b922b204a39
AlphaGHX <ChenYuanHangOL@outlook.com>
2022-02-24 16:55:52 +0800

    更改了cors路由

--- initialize/router.go
+++ initialize/router.go
@@ -5,14 +5,14 @@ import (
 	"blog/routers"
 	"fmt"
 
-	"github.com/gin-contrib/cors"
+	// "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Routers() *gin.Engine {
 	fmt.Println("INFO\t启动路由...")
 	Router := gin.Default()
-	Router.Use(cors.Default())
+	// Router.Use(cors.Default())
 
 	systemRouter := routers.RouterGroupApp.System
 
