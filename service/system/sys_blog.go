7e838ff7f0c59d13d60c170312763e06dbb86a76
AlphaGHX <ChenYuanHangOL@outlook.com>
2022-05-09 20:18:02 +0800

    fix: 修复输入任意url导致数据库异常写入的问题

--- service/system/sys_blog.go
+++ service/system/sys_blog.go
@@ -3,6 +3,7 @@ package system
 import (
 	"blog/global"
 	"blog/models"
+	"errors"
 )
 
 type BlogService struct {
@@ -11,6 +12,9 @@ type BlogService struct {
 func (s *BlogService) GetBlogInfo(param string) (info models.Blog, err error) {
 	var data models.Blog
 	result := global.GROM.Where("name = ?", param).Find(&data)
+	if result.RowsAffected == 0 {
+		return data, errors.New("nullRows")
+	}
 	if result.Error == nil {
 		data.Views++
 		global.GROM.Save(&data)
